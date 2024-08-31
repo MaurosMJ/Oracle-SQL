d6bfa2388948bda75c2c7f77c0dd326ad238650e
Mauros Milach Junior <mauros99milach@gmail.com>
2024-08-30 23:08:31 -0300

    feat: Activity 1 - Displaying data from multiple tables.sql

--- Fundamentals - Displaying data from multiple tables/Activity 1 - Displaying data from multiple tables.sql
+++ Fundamentals - Displaying data from multiple tables/Activity 1 - Displaying data from multiple tables.sql
@@ -61,6 +61,19 @@ FROM
     where salary between 10000 and 30000;
 
 /*
+Self JOIN com cláusula ON
+Utilizado para quando a condição de ligação está na mesma tabela apenas
+*/
+SELECT
+    emp.first_name AS "Nome empregado",
+    emp.last_name  AS "Sobrenome empregado",
+    man.first_name AS "Nome Gerente",
+    man.last_name  AS "Sobrenome Gerente"
+FROM
+         employees emp
+    JOIN employees man ON ( emp.employee_id = man.manager_id );
+
+/*
 - Natural JOIN se baseia nas colunas com o mesmo nome em duas tabelas
 - As colunas devem ter o mesmo nome e dados iguais
 - Employees.department_id = Departments.department_id
