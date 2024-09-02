f0f5f06a1ecc8ba30bbc777c8c08edeaf389620f
Mauros Milach Junior <mauros99milach@gmail.com>
2024-09-02 09:57:04 -0300

    Refactor: Activity 1 - GROUP BY.sql

---
+++ Fundamentals - Group functions (GROUP BY, HAVING)/Activity 1 - GROUP BY.sql
@@ -0,0 +1,22 @@
+--@Author: MaurosMJ
+
+/*
+Funções de agrupamento pode ser combinado com a cláusula GROUP BY
+*/
+select nvl(to_char(department_id),'Sem id') as "ID do departamento", job_id as "Id do trabalho", to_char(avg(nvl(salary,0)), 'L99G999G999D99') as "Salário" from employees group by department_id, job_id order by "Salário" desc;
+select department_id, job_id, sum(salary) from employees group by department_id, job_id;
+
+/*
+Cláusula WHERE que referencia um grupo gera erro em tela
+O Exemplo abaixo gera o erro: ORA-00934: a função de grupo não é permitida aqui
+*/
+select department_id, max(salary) as "Salario Maximo"
+from employees
+where max(salary) > 10000
+group by department_id;
+
+/*
+Aninhando funções de grupo
+Funções de grupo pode ser aninhado apenas uma vez
+*/
+select max(avg(salary)) as "Média Maxima por ID" from employees group by department_id;
