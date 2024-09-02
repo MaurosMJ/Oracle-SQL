aee04be9141ad61295d17dc7e040894cdf829488
Mauros Milach Junior <mauros99milach@gmail.com>
2024-09-02 16:36:57 -0300

    Refactor Activity 5 - CONCAT.sql

---
+++ Fundamentals - Using SINGLE ROW Functions (SUBSTR, CONCAT, LENGHT, INSTR & Others)/Activity 5 - CONCAT.sql
@@ -0,0 +1,8 @@
+--@Author: MaurosMJ
+
+/*
+Utilizando o 'CONCAT' (Concatena duas strings)
+CONCAT(string, string)
+*/
+--Retorno 'Ola tudo bem? Tudo certo!'
+select concat('Ola tudo bem? ', 'Tudo certo!') from dual;
