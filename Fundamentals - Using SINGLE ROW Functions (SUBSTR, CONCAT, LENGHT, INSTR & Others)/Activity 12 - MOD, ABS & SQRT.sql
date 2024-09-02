abd6a5945343012ff9f03d80824fe6d14c2e0b97
Mauros Milach Junior <mauros99milach@gmail.com>
2024-09-02 16:44:00 -0300

    Refactor Activity 12 - MOD, ABS & SQRT.sql

---
+++ Fundamentals - Using SINGLE ROW Functions (SUBSTR, CONCAT, LENGHT, INSTR & Others)/Activity 12 - MOD, ABS & SQRT.sql
@@ -0,0 +1,22 @@
+--@Author: MaurosMJ
+
+/*
+Utilizando o 'MOD' (Resto da divisão)
+MOD(Dividendo, Divisor)
+*/
+--Retorna: 100
+select MOD(1300, 600) from dual;
+
+/*
+Utilizando o 'ABS' (Valor absoluto)
+ABS(Numero Real)
+*/
+--Retorna: 1300,567
+select ABS(-1300.567) from dual;
+
+/*
+Utilizando o 'SQRT' (Raiz quadrada)
+SQRT(NUMERO)
+*/
+--Retorna: 3
+select SQRT(9) from dual;
